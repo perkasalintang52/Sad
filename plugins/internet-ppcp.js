dac3d6f4f9f80710cafcc4506be0ecfd55294a5d
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-17 16:06:39 +0700

    Update internet-ppcp.js

--- plugins/internet-ppcp.js
+++ plugins/internet-ppcp.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command }) => {
-  let res = await fetch('https://caliphapi.com/api/ppcouple?apikey=ELYASXD')
+  let res = await fetch('https://caliphapi.com/api/ppcouple?apikey=LQzZsX3D')
   if (res.status != 200) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
