2693b0b39df5b72bf928af35e77211454c45e925
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-19 05:50:50 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,5 +1,5 @@
 let handler = async(m, { conn }) => {
-conn.send2Contact(m.chat, numberowner, nameowner, m)
+conn.sendContact(m.chat, numberowner, nameowner, m)
 conn.sendContact(m.chat, numberowner2, nameowner2, m)
 }
 handler.help = ['owner', 'creator']
