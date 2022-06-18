5acfb00e412aa8842b40ee04325ac260bf4057c0
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-19 05:38:42 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,6 +1,14 @@
-let handler = async(m, { conn }) => {
-conn.sendContact(m.chat, numberowner, nameowner, m)
-conn.sendContact(m.chat, numberowner2, nameowner2, m)
+let handler = async (m, { conn }) => {
+let caption = `-------[*OWNER*]-------
+https://wa.me/6281322667077 (Romli)
+https://wa.me/6281215710462 (Naii)
+
+📮 Note: 
+•》Owner tidak menerima save
+•》Chat aneh" ga bakal di tanggapi
+•》Dilarang Spam or telpon
+_Segitu Ajah_
+conn.sendButton( m.chat, caption, `©️ 2022 ZxyuuBotz`, `Omke Banh🖒`, `thanks`, m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
