b24312df34edf7d4181d3021886819e8d13af0e3
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-19 12:56:06 +0700

    Update fun-darkjoke.js

--- plugins/fun-darkjoke.js
+++ plugins/fun-darkjoke.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-let rest = 'https://caliphapi.com/api/darkjokes?apikey=fYvaxGEp'
+let rest = 'https://caliphapi.com/api/darkjokes?apikey=YwWDHebP'
     conn.sendButtonImg(m.chat, rest, 'Dark? 🤨', wm2, 'NEXT', '.darkjokes', m)
 }
 handler.help = ['darkjokes']
