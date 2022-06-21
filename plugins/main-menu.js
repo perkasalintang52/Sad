57b56b98bfd135cc13b2ed69fa73b35285dbb2b1
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-21 20:09:00 +0700

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -304,7 +304,7 @@ const template = generateWAMessageFromContent(m.key.remoteJid, proto.Message.fro
             description: menuu,
             buttonText: 'KLIK AJG🗿',
             listType: 1,
-            footerText: "Zxyuu Romm -- Xzyuu Naii ]",
+            footerText: "Zxyuu Romm -- Xzyuu Naii",
             mtype: 'listMessage',
             sections: [
               {
