9a7236a65c08095ae7e6504738740754cec6cb2d
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-21 07:29:41 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -4,10 +4,10 @@ let handler = async(m, { conn }) => {
 	let id = m.chat
 	ftroli = { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: 9999999999999999999999999999999999999999999999999999999, status: 1, surface: 1, message: wm, orderTitle: wm, sellerJid: '0@s.whatsapp.net' } } }
 	fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': require('fs').readFileSync('./src/logo.jpg'), thumbnail: require('fs').readFileSync('./src/logo.jpg'),sendEphemeral: true}}}
-	/*conn.p[id] = [
+	conn.p[id] = [
 	await conn.sendContact(m.chat, numberowner, nameowner, { contexInfo: { forwardingScore: 99999, isForwarded: true } })
               conn.sendContact(m.chat, numberowner2, nameowner2, { contexInfo: { forwardingScore: 99999, isForwarded: true })
-        ]*/
+        ]
 	await delay(100)
   return conn.sendMessage(m.chat, { text: `*Hallo kak* @${await m.sender.split('@')[0]},
 -----[Owner]-----
