3f2283dbd8d9a7bc6f63519e00d297f3b8614855
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-21 13:34:57 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -15,7 +15,7 @@ Sc Ori By Hizer Official :
 https://youtu.be/AXPzjQ5jxx8
 
 Sc Recode By Romli:
-https://wa.me/6281322667077?text=Bang+Bagi+Sc+🗿
+https://wa.me/6281322667077?text=Bang+Bagi+Sc
 
 _ZxyuuBotz_`
 conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
