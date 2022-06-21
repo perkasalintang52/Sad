f4e5b11b3cf47515c800703cdd83b8e2785469c9
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-21 18:19:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -66,7 +66,6 @@ global.APIs = { // API Prefix
   lolhuman: 'https://api.lolhuman.xyz',
   LeysCoder: 'https://leyscoders-api.herokuapp.com',
   rey: 'https://server-api-rey.herokuapp.com'
-  zxyuu: 'https://yog-apikey.herokuapp.com'
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
