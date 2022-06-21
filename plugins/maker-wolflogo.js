fc92f99fb859718fa17fe6d797987f19b61b150b
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-21 18:32:25 +0700

    Update maker-wolflogo.js

--- plugins/maker-wolflogo.js
+++ plugins/maker-wolflogo.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Parameter'
   m.reply('*[ ❗ ] Wait,Proses...*')
-  let res = `https://api.zeks.xyz/api/wolflogo?apikey=apivinz&text1=${response[0]}&text2=${response[1]}`
+  let res = `https://yog-apikey.herokuapp.com/api/textpro/logo-wolf?apikey=YogGanz&text=Yogga&text2=Ganz`
   conn.sendFile(m.chat, res, 'wolflogo.jpg', `Nih kak`, m, false)
 }
 handler.help = ['wolflogo'].map(v => v + ' <text|text>')
