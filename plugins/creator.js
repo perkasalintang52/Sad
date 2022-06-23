0e0341bd4a24e2f643030323a79c3df4425205c6
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-23 09:17:53 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,25 +5,9 @@ VERSION:3.0
 N:Sy;Bot;;;
 FN: ZxyuuRomm
 item.ORG: ytta!
-item1.TEL;waid=6281322667077:6281322667077@s.whatsapp.net
+item1.TEL;waid=6281322667077:6281215710461@s.whatsapp.net
 item1.X-ABLabel:Sibuk 
 item2.EMAIL;type=INTERNET:Eitss
-item2.X-ABLabel:Email
-item3.ADR:;;🇮🇩Bandung Barat;;;;
-item3.X-ABADR:ac
-item3.X-ABLabel:📍 LOCATION
-item4.URL:https://github.com/romlisuhanda
-item4.X-ABLabel:Website
-END:VCARD`
-
-const vcard = `BEGIN:VCARD
-VERSION:3.0
-N:Sy;Bot;;;
-FN: XzyuuNaii
-item.ORG: ytta!
-item1.TEL;waid=6281215710461:6281215710461@s.whatsapp.net
-item1.X-ABLabel:Sibuk 
-item2.EMAIL;type=INTERNET:Eitsss
 item2.X-ABLabel:Email
 item3.ADR:;;🇮🇩Indonesia;;;;
 item3.X-ABADR:ac
@@ -40,7 +24,7 @@ const sentMsg  = await conn.sendMessage(
         }
     }
 )
-await conn.reply(m.chat, "itu Nomor Owner", sentMsg)}
+await conn.reply(m.chat, "Ini Owner Saya Bang\n\nhttps://wa.me/6281322667077\n\nhttps://wa.me/6281215710461", sentMsg)}
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
 
