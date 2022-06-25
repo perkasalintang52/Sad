a1830d18ac1ceca67ec2eb39fbd4a44a2642e483
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-26 05:29:17 +0700

    Update owner-bcgc.js

--- plugins/owner-bcgc.js
+++ plugins/owner-bcgc.js
@@ -4,13 +4,13 @@ let handler = async (m, { conn, isROwner, text }) => {
     let groups = Object.entries(getGroups).slice(0).map(entry => entry[1])
     let anu = groups.map(v => v.id)
     let romli = 'https://telegra.ph/file/b17f0ba47c63bcfadc3df.jpg'
-    let zxyuu = `*-----------[BroadCast Group]------------*`.trim()
+    let str = `*-----------[BroadCast Group]------------*`.trim()
     var pesan = m.quoted && m.quoted.text ? m.quoted.text : text
     if(!pesan) throw 'teksnya?'
     m.reply(`Mengirim Broadcast Ke ${anu.length} Chat, Waktu Selesai ${anu.length * 0.5 } detik`)
     for (let i of anu) {
     await delay(500)
-    conn.send2ButtonImg(i, romli, zxyuu, `${pesan}`, 'Created By romli', 'OWNER', '.owner', 'MENU', '.menu all', null).catch(_ => _)
+    conn.send2ButtonImg(i, romli, str, `${pesan}`, wm, 'OWNER', '.owner', 'MENU', '.menu all', null).catch(_ => _)
     }
   m.reply(`Sukses Mengirim Broadcast Ke ${anu.length} Group`)
 }
