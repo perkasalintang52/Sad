14ff858a28d55173ef2e21a01ae282e0a442c5ce
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-29 19:48:19 +0700

    Update main-hai.js

--- plugins/main-hai.js
+++ plugins/main-hai.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/abff2f2c3398c30703d47.png', m, { packname: "Stiker", author: "By ZxyuuBotz" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/c7c5ab6aa01d3d4308d57.png', m, { packname: "Stiker", author: "By ZxyuuBotz" })
 }
 
 handler.customPrefix = /^(hai|haii|halo|hallo|bot|botz)$/i
