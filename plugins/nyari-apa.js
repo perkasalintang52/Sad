7d7f3054bc3b2be3e1bad0bf56e496ff5d82d12b
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-29 19:06:32 +0700

    Update nyari-apa.js

--- plugins/nyari-apa.js
+++ plugins/nyari-apa.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
 let name = m.fromMe ? conn.user : conn.contacts[m.sender]
-let zxyuu = 'https://telegra.ph/file/74f37add1d7a2535c58eb.jpg'
-await conn.send2ButtonImg(m.chat, zxyuu, "*Mau Apa Tag owner Gua Ngab?🤨*", '*📮: Ada yang bisa saya bantu?*', 'OWNER', '.owner', 'KEMBALI', '.menu', m)
+let zxyuu = 'https://telegra.ph/file/0487cb2df67c66d2ed7c5.jpg'
+await conn.send2ButtonImg(m.chat, zxyuu, "*Mau Apa Tag owner Gua Ngab????🤨*", '*📮: Ada yang bisa saya bantu?*', 'OWNER', '.owner', 'KEMBALI', '.menu', m)
                         
 }
 handler.customPrefix = /@6281322667077/i
