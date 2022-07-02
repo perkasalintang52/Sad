0d5d1045257a1ca5f6333a7ebefd9b1544d31069
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-07-02 10:52:16 +0700

    Update smeme.js

--- plugins/smeme.js
+++ plugins/smeme.js
@@ -1,6 +1,4 @@
 const uploadImage = require('../lib/uploadImage')
-const { MessageType } = require('@adiwajshing/baileys')
-const { sticker } = require('../lib/sticker')
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
     let [atas, bawah] = text.split`|`
@@ -10,21 +8,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
     let img = await q.download()
     let url = await uploadImage(img)
-    meme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas ? atas : '')}/${encodeURIComponent(bawah ? bawah : '')}.png?background=${url}`
-try {
-    let stiker = await sticker(null, meme, global.packname, global.author)
-    await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
-      quoted: m
-    })
-  } catch (e) {
-    m.reply('gagal membuat stiker, Mencoba Mengirim gambar')
-    await conn.sendFile(m.chat, meme, 'image.png', 'Nih Banh', m)
-  }
-}
-handler.help = ['smeme<teks atas>|<teks bawah>']
-handler.tags = ['sticker']
-handler.command = /^(smeme)$/i
+    let meme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas ? atas : '')}/${encodeURIComponent(bawah ? bawah : '')}.png?background=${url}`
+    conn.sendStimg(m.chat, meme, m, { packname: packname, author: author })
 
-handler.limit = false
+}
+handler.help = ['stickermeme <teks>|<teks>']
+handler.tags = ['sticker']
+handler.command = /^(s(tic?ker)?me(me)?)$/i
+
+handler.limit = true
 
 module.exports = handler
