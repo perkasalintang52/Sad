51389f8c14d81421a4fc54411e1c923eb4c21c58
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-07-02 10:56:58 +0700

    Update smeme.js

--- plugins/smeme.js
+++ plugins/smeme.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     let img = await q.download()
     let url = await uploadImage(img)
     let meme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas ? atas : '')}/${encodeURIComponent(bawah ? bawah : '')}.png?background=${url}`
-    conn.sendStimg(m.chat, meme, m, { packname: packname, author: author })
+    conn.sendFile(m.chat, meme, m, { packname: packname, author: author })
 
 }
 handler.help = ['stickermeme <teks>|<teks>']
